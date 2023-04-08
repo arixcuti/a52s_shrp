45f562740dcce48ecf2709c8a395722a005f9b66
arixcute <125747568+arixcuti@users.noreply.github.com>
2023-04-08 10:47:53 +0000

    Upload File

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# For building with minimal manifest
+export ALLOW_MISSING_DEPENDENCIES="true"
+export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.qcom.rc for more information
